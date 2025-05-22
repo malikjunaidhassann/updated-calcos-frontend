52b184cb77659845f811aeaa6c0889705e8be501
malikjunaidhassann <140273527+malikjunaidhassann@users.noreply.github.com>
2025-05-22 14:07:40 +0500

    change the condition of price

--- src/components/utilsElements.tsx
+++ src/components/utilsElements.tsx
@@ -8,7 +8,7 @@ const priceFormatter = new Intl.NumberFormat("en-US", {
 
 function PriceElement({ price, currency }: any) {
   try {
-    price = priceFormatter.format(price.match(/[\d,.]+/)[0].replace(/,/g, ''));
+    price = priceFormatter.format(price.replace(/[\s\u00A0\u202F]/g, '').match(/[\d,.]+/)[0].replace(/,/g, ''));
   } catch (error) {
     console.error("Error formatting price:", error);
   }
