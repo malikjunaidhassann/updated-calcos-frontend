4375b2d28c7ea07780b8c5a96847e4f4b7b24230
malikjunaidhassann <140273527+malikjunaidhassann@users.noreply.github.com>
2025-06-12 18:35:55 +0500

    sorted the result from low to high prices

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -3,33 +3,37 @@ import "../assets/product-card.css";
 import InputProductCard from "./InputProductCard.js";
 import ProductCard from "./ProductCard.js";
 import LoadingText from "./LoadingText.js";
-import { Card } from "./ui/card.js";
 
-function isValidInput(inputProduct: any){
+function isValidInput(inputProduct: any) {
   return inputProduct.imageUrl && inputProduct.imageUrl !== "NA";
 }
 
 function Results({ inputProduct, similarProducts, isLoading }: any) {
-  if(isValidInput(inputProduct)){
+  if (isValidInput(inputProduct)) {
+    similarProducts.sort((a: any, b: any) => {
+      const aVal = a?.price?.extracted_value ?? 0;
+      const bVal = b?.price?.extracted_value ?? 0;
+      return aVal - bVal;
+    });
     return (
       <>
         <div className="results-container !mt-3">
           {/* Display the input product */}
           {inputProduct && (
-              <InputProductCard product={inputProduct} />
+            <InputProductCard product={inputProduct} />
           )}
-          
+
           <hr className="rounded" />
 
           {/* Display similar products */}
           {similarProducts && similarProducts.length > 0 && (
             <div className="similar-products">
               <h2>Similar Products</h2>
-              <div  className="flex flex-wrap  justify-center items-center gap-[42px] max-w-[full]">
-              {similarProducts.map((product: any, i: any) => (
+              <div className="flex flex-wrap  justify-center items-center gap-[42px] max-w-[full]">
+                {similarProducts.map((product: any, i: any) => (
                   <ProductCard key={i} product={product} />
                 ))}
-                </div>
+              </div>
             </div>
           )}
           {isLoading !== "DONE" && <LoadingText />}
@@ -37,11 +41,11 @@ function Results({ inputProduct, similarProducts, isLoading }: any) {
       </>
     );
   }
-  return(
+  return (
     <>
       <div className="results-container !mt-3">
         <h1>Invalid data, Please try again.</h1>
-      </div> 
+      </div>
     </>
   )
 
